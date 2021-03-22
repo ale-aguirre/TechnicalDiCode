fcbf5b078827f2d89f13d0a53fcd98a41d1f7b38
ale-aguirre <ale.aguirre.2222@hotmail.com>
2021-03-22 03:20:15 -0300

    update v1

--- src/firebase.js
+++ src/firebase.js
@@ -1,10 +1,10 @@
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: "AIzaSyCojDonaoLOGKRx2JtLeej8d44yqi3RF7k",
-    authDomain: "technicaldicode.firebaseapp.com",
-    projectId: "technicaldicode",
-    storageBucket: "technicaldicode.appspot.com",
-    messagingSenderId: "563941886946",
-    appId: "1:563941886946:web:0d12b9e62c0fd611ddc7bb",
-    measurementId: "G-6TL5P70QEP"
-  };
+  apiKey: "AIzaSyCh1W5PY4R9T0uJwWO6kVRTyl8-sQcW1cM",
+  authDomain: "technicaldicode-bb8e0.firebaseapp.com",
+  projectId: "technicaldicode-bb8e0",
+  storageBucket: "technicaldicode-bb8e0.appspot.com",
+  messagingSenderId: "316558717682",
+  appId: "1:316558717682:web:af1b12f9ee35731254166b",
+  measurementId: "G-QYB41VN85R"
+};
